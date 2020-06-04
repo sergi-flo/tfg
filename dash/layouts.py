cd79b306c956b9ad802886b8921ce74f6bf8b437
Sergi Flo <sergifs9@gmail.com>
2020-06-04 12:50:44 +0200

    added grey mesh

--- dash/layouts.py
+++ dash/layouts.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
 
-import inspect
-import os
-import sys
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.graph_objects as go
-
-import ppanda
+import os,sys,inspect
 from app import app
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir) 
+import ppanda
 
 bioart_logo_path='BIOART_logo.png'
 upc_logo_path='logo_UPC.png'
